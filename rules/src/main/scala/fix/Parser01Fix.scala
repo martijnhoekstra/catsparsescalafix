2f3fc0f6c0bc38e22024e63ca03b1ce6c3d05e8d
Martijn Hoekstra <martijnhoekstra@gmail.com>
2020-12-17 15:31:44 +0100

    refactor to use symbol matchers

--- rules/src/main/scala/fix/Parser01Fix.scala
+++ rules/src/main/scala/fix/Parser01Fix.scala
@@ -6,22 +6,48 @@ import scala.meta.Decl.Def
 import scala.meta.Type.Apply
 
 class Parser01Fix extends SemanticRule("Parser01Fix") {
-  
-  val parserModuleRenames = List(
-    "string1", "ignoreCase1", "oneOf1", "map1", "select1", "charsWhile1",
-    "until1", "void1", "backtrack1", "as1", "unmap1", "compute1", "defer1",
-    "rep1Sep", "repAs1", "rep1", "length1")
-    .flatMap(oldone => List(
-       oldone -> oldone.replace("1", ""),
-       oldone.replace("1", "") -> oldone.replace("1", "0")
-    )).toMap ++ List("flatMap10" -> "flatMap", "flatMap" -> "flatMap0").toMap
-  val pClassRenames = Map(
+
+  val parserModuleReplacements = {
+    val renames = List("stringX","ignoreCaseX","oneOfX","mapX",
+      "selectX","charsWhileX","untilX","voidX","backtrackX","asX","unmapX",
+      "computeX","deferX","repXSep","repAsX","repX","lengthX")
+      .flatMap(old =>
+        List(
+          old.replace("X", "1") -> old.replace("X", ""),
+          old.replace("X",  "") -> old.replace("X", "0")
+        )
+      )
+      .toMap ++
+      Map("flatMap10" -> "flatMap", "flatMap" -> "flatMap0")
+
+    renames.map { case (from, to) => s"cats/parse/Parser.$from" -> to
+    }
+  }
+
+  object RenamedParserModuleMethodSelect {
+    def unapply(t: Tree)(implicit doc: SemanticDocument): Option[String] = t match {
+      case Term.Select(_, Name(_)) => parserModuleReplacements.get(t.symbol.value.takeWhile(_ != '('))
+      case _ => None
+    }
+  }
+
+  val pClassRenames = List(
     "orElse" -> "orElse0",
     "orElse1" -> "orElse",
     "rep" -> "rep0",
-    "rep1" -> "rep",
+    "rep1" -> "rep"
+  ).flatMap{
+    case (k, v) => List(s"cats/parse/Parser0#$k", s"cats/parse/Parser1#$k").map(_ -> v)
+  }.toMap
 
-  )
+  object RenamedParserInstanceMethod {
+    def unapply(t: Tree)(implicit doc: SemanticDocument): Option[String] = t match {
+      case Name(_) => pClassRenames.get(t.symbol.value.takeWhile(_ != '('))
+      case _ => None
+    }
+  }
+
+
 
   override def fix(implicit doc: SemanticDocument): Patch = {
     var p0ClassName = "Parser0"
@@ -29,62 +55,46 @@ class Parser01Fix extends SemanticRule("Parser01Fix") {
     def pModuleName = p1ClassName
 
     object ParserTpeFor {
+      //I have no idea what I'm doing
       def unapply(t: Tree): Option[String] = t match {
-        case tpe @ Type.Name(_) => tpe.symbol.info match {
-          case Some(si) if si.isClass => {
-            if(si.toString().startsWith("cats/parse/Parser# =>")) //I have no idea what I'm doing
-              Some(p0ClassName)
-            else if(si.toString().startsWith("cats/parse/Parser1# =>"))
-              Some(p1ClassName)
-            else
-              None
+        case tpe @ Type.Name(_) =>
+          tpe.symbol.info match {
+            case Some(si) if si.isClass => {
+              if (si.toString().startsWith("cats/parse/Parser# =>"))
+                Some(p0ClassName)
+              else if (si.toString().startsWith("cats/parse/Parser1# =>"))
+                Some(p1ClassName)
+              else
+                None
+            }
+            case _ => None
           }
-          case _ => None
-        }
         case _ => None
       }
     }
 
-    object SemanticParserType {
-      def unapply(tpe: SemanticType): Boolean = tpe match {
-        case TypeRef(_, symbol, _) =>
-             symbol.toString == "cats/parse/Parser1#" ||
-             symbol.toString == "cats/parse/Parser#"
-        case _ => false
-      }
-    }
-
     doc.tree.collect {
-      case Importer(q"cats.parse", importees) => importees.collect {
-        case imp @ Importee.Rename(Name("Parser"), Name(target))  =>
-          p0ClassName = target + "0"
-          Patch.replaceTree(imp, s"Parser0 => $p0ClassName")
-        case imp @ Importee.Name(Name("Parser")) => {
-          p0ClassName = "Parser0"
-          Patch.replaceTree(imp, "Parser0")
-        }
-        case imp @ Importee.Rename(Name("Parser1"), Name(target)) => {
-          p1ClassName = if (target.endsWith("1")) target.init else target
-          Patch.replaceTree(imp, s"Parser => ${p1ClassName}")
-        }
-        case imp @ Importee.Name(Name("Parser1")) => {
-          p1ClassName = "Parser"
-          Patch.replaceTree(imp, "Parser")
-        }
-      }.asPatch
-      case sel @ Term.Select(from, t @ Term.Name(nme)) => {
-        if (from.symbol.toString() == "cats/parse/Parser.")
-          parserModuleRenames.get(nme).map(newName =>
-            Patch.replaceTree(sel, s"$pModuleName.$newName")).getOrElse(Patch.empty)
-        else
-          from.symbol.info.map(_.signature).collect {
-            case MethodSignature(_, _, SemanticParserType()) =>
-              pClassRenames.get(nme).map(newName => Patch.replaceTree(t,newName))
-          }.flatten.getOrElse(Patch.empty)
-      }
-      case tpe @ ParserTpeFor(to) => {
-        Patch.replaceTree(tpe, to)
-      }
+      case Importer(q"cats.parse", importees) =>
+        importees.collect {
+          case imp @ Importee.Rename(Name("Parser"), Name(target)) =>
+            p0ClassName = target + "0"
+            Patch.replaceTree(imp, s"Parser0 => $p0ClassName")
+          case imp @ Importee.Name(Name("Parser")) => {
+            p0ClassName = "Parser0"
+            Patch.replaceTree(imp, "Parser0")
+          }
+          case imp @ Importee.Rename(Name("Parser1"), Name(target)) => {
+            p1ClassName = if (target.endsWith("1")) target.init else target
+            Patch.replaceTree(imp, s"Parser => ${p1ClassName}")
+          }
+          case imp @ Importee.Name(Name("Parser1")) => {
+            p1ClassName = "Parser"
+            Patch.replaceTree(imp, "Parser")
+          }
+        }.asPatch
+      case t @ RenamedParserInstanceMethod(newName) => Patch.replaceTree(t, newName)
+      case t @ RenamedParserModuleMethodSelect(newName) => Patch.replaceTree(t, s"$pModuleName.$newName")
+      case tpe @ ParserTpeFor(to) => Patch.replaceTree(tpe, to)
     }.asPatch
   }
 }
